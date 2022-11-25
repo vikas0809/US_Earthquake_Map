9bcd9242792daf089affd3783e648424b1667106
vikas0809 <vikasjoshi93@live.com>
2022-11-25 12:28:11 -0500

    Added points and circles

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,9 +1,12 @@
 // Create our initial map object
 // Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map", {
+var map = L.map('map', {
   center: [45.52, -122.67],
   zoom: 13
 });
+
+
+
 
 // Add a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
@@ -12,6 +15,18 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "mapbox/dark-v10",
   accessToken: API_KEY
-}).addTo(myMap);
+}).addTo(map);
+
+
+
+
+//  Add a marker to the map for Los Angeles, California.
+//L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "white",
+  fillColor:"ffffa1"
+}).addTo(map);
