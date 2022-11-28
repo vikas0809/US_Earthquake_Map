d5c731172121b02d04e11079413449682f10607e
vikas0809 <vikasjoshi93@live.com>
2022-11-28 12:49:12 -0500

    Layers and popups

--- Simple_Map/static/js/cities.js
+++ Simple_Map/static/js/cities.js
@@ -30,4 +30,22 @@ let cities = [{
     population: 1660272
   }
   ];
-  
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"14",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};  
