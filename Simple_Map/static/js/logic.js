c5818139909ff80f315d103aa305bae8b2e17b26
vikas0809 <vikasjoshi93@live.com>
2022-11-28 13:26:40 -0500

    toronto neighbourhood ploygon

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -5,32 +5,33 @@
 // Add a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
   // Define streetmap and darkmap layers
-  var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/light-v11',
+    id: 'mapbox/streets-v12',
     accessToken: API_KEY
   });
 
-  var darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  var satteliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v12',
     accessToken: API_KEY
   });
 
-let routeData="https://raw.githubusercontent.com/vikas0809/US_Earthquake_Map/Mapping_Single_Points/Simple_Map/static/js/torontoRoutes.json";
+//let routeData="https://raw.githubusercontent.com/vikas0809/US_Earthquake_Map/Mapping_Single_Points/Simple_Map/static/js/torontoRoutes.json";
+let neighbourhoodData="https://raw.githubusercontent.com/vikas0809/US_Earthquake_Map/Mapping_GeoJSON_LineStrings/Simple_Map/static/js/torontoNeighborhoods.json";
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Light: lightmap,
-  Dark: darkmap
+  Streets: streets,
+  Sattelite : satteliteStreets
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map("map", {
-  center: [44.0, -80.0],
-  zoom: 4,
-  layers: [lightmap]
+  center: [43.7, -79.3],
+  zoom: 10,
+  layers: [satteliteStreets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -39,19 +40,21 @@ L.control.layers(baseMaps).addTo(map);
 // Create a style for the lines.
 let myStyle = {
   color: "#ffffa1",
-  weight: 2
+  weight: 1,
+  fillColor : "yellow"
+  
 }
 
 // Grabbing our GeoJSON data.
-d3.json(routeData).then(function(data) {
+d3.json(neighbourhoodData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data,{
   style: myStyle,
   onEachFeature: function(feature, layer) {
     console.log(layer);
-    layer.bindPopup("<h2>"+"Airline: "+feature.properties.airline+" "+feature.properties.airline_id
-                    +"</h2><hr> Destination: "+feature.properties.dst
+    layer.bindPopup("<h2>"+"Neighbourhood: "+feature.properties.AREA_NAME
+                    +"</h2><hr> Neighbourhood Code: "+feature.properties.AREA_S_CD
     );
    }
 }).addTo(map);
